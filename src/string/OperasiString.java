8f64389e4acb8de71d815f564fdb0149a03f0d1d
ydhstr <81416846+ydhstr@users.noreply.github.com>
2021-04-12 15:35:14 -0700

    toUpperCase untuk ... toLowerCase untuk ... substring(5) untuk ... substring(0,4) untuk ...

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -1,13 +1,20 @@
 package string;
 
+import java.util.Locale;
+
 public class OperasiString {
     public static void main(String[] args) {
-        String kota = " Yogyakarta";
+        String kota = "Yogyakarta";
 
         System.out.println(kota);
 
         char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
     }
 }
